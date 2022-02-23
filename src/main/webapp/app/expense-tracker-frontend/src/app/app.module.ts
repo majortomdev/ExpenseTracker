9e3b0d901a6b298d8611e4053964ebaf9453edff
Joseph Kinahan <kinahanj25@gmail.com>
2022-02-23 00:53:21 +0000

    httpclient and service class retriveing from b/e via url

--- src/main/webapp/app/expense-tracker-frontend/src/app/app.module.ts
+++ src/main/webapp/app/expense-tracker-frontend/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ListExpenseComponent } from './components/list-expense/list-expense.component';
+import { ExpenseService } from './services/expense.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { ListExpenseComponent } from './components/list-expense/list-expense.com
     ListExpenseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ExpenseService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main/webapp/app/expense-tracker-frontend/src/app/services/expense.service.ts
+++ src/main/webapp/app/expense-tracker-frontend/src/app/services/expense.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Expense } from '../models/expense';
+import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ExpenseService {
 
-  constructor() { }
+
+  private getUrl: string = "http://localhost:8080/api/v1/expenses";
+
+  constructor(private _httpClient: HttpClient) { }
+
+  getExpenses(): Observable<Expense[]> {
+    return this._httpClient.get<Expense[]>(this.getUrl).pipe(
+      map((response: any) => response)
+    )
+  }
 }
